17bc274da315111f7fe6f6e76bb1a996a851627e
Vladislav Surin <vlad_surin@mail.ru>
2022-10-05 11:09:10 +0300

    added script for timer

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/* swiper */
 const swiper = new Swiper(".swiper", {
   slidesPerGroup: 1,
   slidesPerView: 1,
@@ -14,3 +15,41 @@ const swiper = new Swiper(".swiper", {
     prevSlideMessage: "Предыдущий слайд",
   },
 });
+
+/* timer */
+const minutesVal = document.querySelector(".timer__minutes .timer__value");
+const secondsVal = document.querySelector(".timer__seconds .timer__value");
+const minutesText = document.querySelector(".timer__minutes .timer__text");
+const secondsText = document.querySelector(".timer__seconds .timer__text");
+
+let time = 1800;
+
+updateTimer();
+setInterval(updateTimer, 1000);
+
+function declOfNum(number, titles) {
+  cases = [2, 0, 1, 1, 1, 2];
+  return titles[
+    number % 100 > 4 && number % 100 < 20
+      ? 2
+      : cases[number % 10 < 5 ? number % 10 : 5]
+  ];
+}
+
+function updateTimer() {
+  const minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+  minutesVal.textContent = minutes;
+  secondsVal.textContent = seconds;
+
+  minutesText.textContent = declOfNum(minutes, ["минута", "минуты", "минут"]);
+  secondsText.textContent = declOfNum(seconds, [
+    "секунда",
+    "секунды",
+    "секунд",
+  ]);
+  time--;
+}
